6f60a631c958d77e8b09cf24082527c9fc557322
Shivam00002 <dubey02shiv@gmail.com>
2024-09-22 10:06:34 +0530

    resolved cors error

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
 const connectDB = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
@@ -10,19 +11,22 @@ const app = express();
 
 connectDB();
 
+
+
 const corsOptions = {
-    origin: [process.env.FRONTEND_URL, "books-management-ocb5.vercel.app", "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  };
+  origin: [process.env.FRONTEND_URL, "https://books-management-ocb5.vercel.app", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
 
 
 app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Books Management API 🔥📚');
+  res.send('Welcome to the Books Management API 🔥📚');
 });
 
 
